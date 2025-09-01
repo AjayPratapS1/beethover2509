5194d4ab3220a749692c2f9742b92213a805b799
Ajay Pratap Singh <121428720+AjayPratapS1@users.noreply.github.com>
2025-09-01 10:50:03 +0530

    Learing - Task find_sum

---
+++ 01-09-2025/find_sum.py
@@ -0,0 +1,16 @@
+#find_sum
+'''
+find_sum
+args: firstNum, secondNum
+returns sum of the numbers
+print("hello ajay")
+'''
+
+def find_sum(firstNum, secondNum):
+    result = firstNum + secondNum
+    return result
+
+print(find_sum(10,20))
+print(find_sum(1,20))
+
+
