f892e669928f50d6f51f10b13e7241c1d2b75bee
Ajay Pratap Singh <121428720+AjayPratapS1@users.noreply.github.com>
2025-09-04 12:08:44 +0530

    API Consumption

--- 04-09-2025/client_post.py
+++ 04-09-2025/client_post.py
@@ -3,9 +3,22 @@
 import requests
 BASE_URL = 'https://jsonplaceholder.typicode.com'
 
+#Fetch all post : GET
 posts_response = requests.get(f'{BASE_URL}/posts')
 posts = posts_response.json()
 print("Resopone of API's: ")
-# print(posts) 
-print(posts[1])
+print(posts) 
+
+# read by id : GET /posts/1
+print('Consuming : Read Post By Id == 1...')
+response = requests.get(f'{BASE_URL}/posts/1')
+post = response.json()
+print(post)
+
+# create post : POST /posts {"userId":1, "title":"Some Title", "body" : "Some Body"}
+print('Consuming : create post...')
+post = {"userId":1, "title":"Some Title", "body" : "Some Body"}
+response = requests.post(f'{BASE_URL}/posts', data = post)
+createdPost = response.json()
+print(createdPost)
 
